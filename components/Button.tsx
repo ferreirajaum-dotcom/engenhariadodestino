a1518ef4ee740a33e3fc4abfdb3f36194878514b
ferreirajaum-dotcom <ferreirajaum@icloud.com>
2026-01-06 10:04:23 -0300

    Update Button.tsx

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,8 +1,8 @@
 import React, { useRef, useState } from 'react';
 import { ArrowRight } from 'lucide-react';
-import { motion } from 'framer-motion';
+import { motion, HTMLMotionProps } from 'framer-motion';
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+interface ButtonProps extends HTMLMotionProps<"button"> {
   variant?: 'primary' | 'secondary';
   fullWidth?: boolean;
 }
